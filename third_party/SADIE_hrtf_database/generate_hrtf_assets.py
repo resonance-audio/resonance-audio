ad274d11a6ee4a9bd14477e21b4234fa04bcbe9b
Marcin Gorzel <26985965+mgorzel@users.noreply.github.com>
2019-03-11 13:40:37 +0000

    Correct multi-line comment formatting in generate_hrtf_assets.py.

--- third_party/SADIE_hrtf_database/generate_hrtf_assets.py
+++ third_party/SADIE_hrtf_database/generate_hrtf_assets.py
@@ -1,4 +1,4 @@
-/*
+"""
 Copyright 2018 Google Inc. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,7 @@ distributed under the License is distributed on an "AS-IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
+"""
 
 #!/usr/bin/python
 """Generates a C++ class that statically stores raw asset data.
